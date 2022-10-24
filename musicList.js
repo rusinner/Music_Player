44ab3f6c766736d51b88549dc5450862bff89634
Theologos Mavropoulos <rusinner88@gmail.com>
2022-10-24 15:04:23 +0300

    correct buttons

--- musicList.js
+++ musicList.js
@@ -6,39 +6,23 @@ let allMusic = [
         src:"music-1"
     },
     {
-        name:"Paranoid",
-        artist:"Jordan Schor",
-        img:"music-1",
-        src:"music-1"
+        name:"War Pigs",
+        artist:"Black Sabbath",
+        img:"music-2",
+        src:"music-2"
     },
     {
-        name:"Paranoid",
-        artist:"Jordan Schor",
-        img:"music-1",
-        src:"music-1"
+        name:"Black Sabbath",
+        artist:"Black Sabbath",
+        img:"music-3",
+        src:"music-3"
     },
     {
-        name:"Paranoid",
-        artist:"Jordan Schor",
-        img:"music-1",
-        src:"music-1"
+        name:"Iron Man",
+        artist:"Black Sabbath",
+        img:"music-4",
+        src:"music-4"
     },
-    {
-        name:"Paranoid",
-        artist:"Jordan Schor",
-        img:"music-1",
-        src:"music-1"
-    },
-    {
-        name:"Paranoid",
-        artist:"Jordan Schor",
-        img:"music-1",
-        src:"music-1"
-    },
-    {
-        name:"Paranoid",
-        artist:"Jordan Schor",
-        img:"music-1",
-        src:"music-1"
-    },
+    
+    
 ]
